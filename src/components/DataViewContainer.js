bc8bbbf7e3165fcc5b19babb65c3dd80bfcb7aa5
n6134123 <n6134123@gmail.com>
2017-12-21 19:55:12 -0800

    CountSlider

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { ShotChart } from "./ShotChart";
+import {} from 'antd';
+import { CountSlider } from "./CountSlider";
 
 export class DataViewContainer extends React.Component {
 
     render() {
         return (
-          <ShotChart playerId={this.props.playerId}/>
+            <div className="data-view">
+                <ShotChart playerId={this.props.playerId}/>
+                <div className="filters">
+                    <CountSlider/>
+                </div>
+            </div>
         );
     }
 }
